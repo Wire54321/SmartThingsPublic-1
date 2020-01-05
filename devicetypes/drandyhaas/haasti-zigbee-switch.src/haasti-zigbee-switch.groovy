c75c20eaa70ac2e7c16dc7cee4d7a4035bd310a2
ANDREW HAAS <haas@yahoo.com>
2020-01-04 22:36:33 -0500

    better

--- devicetypes/drandyhaas/haasti-zigbee-switch.src/haasti-zigbee-switch.groovy
+++ devicetypes/drandyhaas/haasti-zigbee-switch.src/haasti-zigbee-switch.groovy
@@ -7,9 +7,28 @@ metadata {
 		capability "Switch"
 		capability "Health Check"
         
-        command "sendtext"
         command "gettext"
-        
+        command "butt1"
+        command "allon"
+        command "on1"
+        command "on2"
+        command "on3"
+        command "on4"
+        command "butt2"
+        command "alloff"
+        command "off1"
+        command "off2"
+        command "off3"
+        command "off4"
+        command "getadc0"
+        command "getadc1"
+        command "getadc4"
+        command "getadc5"
+
+        command "sendtext1"
+
+        attribute "text","string"
+
         fingerprint profileId: "0104", manufacturer: "TexasInstruments", model: "TI0001", deviceJoinName: "HaasTI Thing"
 	}
 
@@ -34,14 +53,70 @@ metadata {
 		standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
 			state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
 		}
-        standardTile("sendtext", "device.sendtext", inactiveLabel: false, width: 2, height: 2) {
-			state "default", label:"sendtext", action:"sendtext"
-		}
         standardTile("gettext", "device.gettext", inactiveLabel: false, width: 2, height: 2) {
 			state "default", label:"gettext", action:"gettext"
 		}
+        standardTile("text", "device.text", inactiveLabel: false, width: 2, height: 2) {
+			state "text", label:'${currentValue}'
+		}
+        
+        standardTile("butt1", "device.butt1", inactiveLabel: false) {
+			state "default", label:"button1", action:"butt1"
+		}
+        standardTile("allon", "device.allon", inactiveLabel: false) {
+			state "default", label:"all on", action:"allon"
+		}
+        standardTile("on1", "device.on1", inactiveLabel: false) {
+			state "default", label:"on1", action:"on1"
+		}
+        standardTile("on2", "device.on2", inactiveLabel: false) {
+			state "default", label:"on2", action:"on2"
+		}
+        standardTile("on3", "device.on3", inactiveLabel: false) {
+			state "default", label:"on3", action:"on3"
+		}
+        standardTile("on4", "device.on4", inactiveLabel: false) {
+			state "default", label:"on4", action:"on4"
+		}
+        
+        standardTile("butt2", "device.butt2", inactiveLabel: false) {
+			state "default", label:"button2", action:"butt2"
+		}
+        standardTile("alloff", "device.alloff", inactiveLabel: false) {
+			state "default", label:"all off", action:"alloff"
+		}
+        standardTile("off1", "device.off1", inactiveLabel: false) {
+			state "default", label:"off1", action:"off1"
+		}
+        standardTile("off2", "device.off2", inactiveLabel: false) {
+			state "default", label:"off2", action:"off2"
+		}
+        standardTile("off3", "device.off3", inactiveLabel: false) {
+			state "default", label:"off3", action:"off3"
+		}
+        standardTile("off4", "device.off4", inactiveLabel: false) {
+			state "default", label:"off4", action:"off4"
+		}
+        
+        standardTile("getadc0", "device.getadc0", inactiveLabel: false) {
+			state "default", label:"getadc0", action:"getadc0"
+		}
+        standardTile("getadc1", "device.getadc1", inactiveLabel: false) {
+			state "default", label:"getadc1", action:"getadc1"
+		}
+        standardTile("getadc4", "device.getadc4", inactiveLabel: false) {
+			state "default", label:"getadc4", action:"getadc4"
+		}
+        standardTile("getadc5", "device.getadc5", inactiveLabel: false) {
+			state "default", label:"getadc5", action:"getadc5"
+		}
+        
+        standardTile("sendtext1", "device.sendtext1", inactiveLabel: false) {
+			state "default", label:"sendtext1", action:"sendtext1"
+		}
+        
 		main "switch"
-		details(["switch", "refresh", "sendtext", "gettext"])
+		details(["switch", "refresh", "gettext", "text", "butt1","allon","on1","on2","on3","on4", "butt2","alloff","off1","off2","off3","off4", "getadc0","getadc1","getadc4","getadc5", "sendtext1"])
 	}
 }
 
@@ -64,6 +139,7 @@ def parse(String description) {
             byte[] asciireadstring = readstring.decodeHex()
             String text = new String(asciireadstring)
             log.debug "readstring is $readstring, ascii $asciireadstring, text $text"
+            return createEvent(name: "text", value: "$text")
 		}
         else {
 			log.warn "Not an attribute we can decode"
@@ -101,27 +177,28 @@ def gettext(){ // read some attribute string from the device
     zigbee.readAttribute(0x000, 0x0010) // gets the last command the device heard us send
 }
 
-def sendtext(){ // set the LocationDescription string on the device
-    //log.debug "sendtext"
+def sendtext1(){ // set the LocationDescription string on the device
+    log.debug "sendtext1"
     //sendtodevice("ping") // to say hi
-    //sendtodevice("arduino1") // to tell the arduino, connected on serial, to do something, like send back a message on serial (it should just make sure it's <16 bytes, and ends with a ".")
-    //sendtodevice("on") // turn on all io/led
-	//sendtodevice("on1") // turn on io/led 1
-	//sendtodevice("on2") // turn on io/led 2
-	//sendtodevice("on3") // turn on io/led 3
-	//sendtodevice("on4") // turn on io/led 4
-	//sendtodevice("off") // turn off all io/led
-	//sendtodevice("off1") // turn off io/led 1
-	//sendtodevice("off2") // turn off io/led 2
-	//sendtodevice("off3") // turn off io/led 3
-	//sendtodevice("off4") // turn off io/led 4
-    //sendtodevice("getbutt1") // whether button 1 is being pressed
-    //sendtodevice("getbutt2") // whether button 2 is being pressed
-    sendtodevice("getadc0") // return 12 bit value of adc 0
-    //sendtodevice("getadc1") // return 12 bit value of adc 1
-    //sendtodevice("getadc4") // return 12 bit value of adc 4
-    //sendtodevice("getadc5") // return 12 bit value of adc 5
+    sendtodevice("arduino1") // to tell the arduino, connected on serial, to do something, like send back a message on serial (it should just make sure it's <16 bytes, and ends with a ".")
 }
+
+def allon(){sendtodevice("on")}
+def on1(){sendtodevice("on1")}
+def on2(){sendtodevice("on2")}
+def on3(){sendtodevice("on3")}
+def on4(){sendtodevice("on4")}
+def alloff(){sendtodevice("off")}
+def off1(){sendtodevice("off1")}
+def off2(){sendtodevice("off2")}
+def off3(){sendtodevice("off3")}
+def off4(){sendtodevice("off4")}
+def butt1(){sendtodevice("getbutt1")}
+def butt2(){sendtodevice("getbutt2")}
+def getadc0(){sendtodevice("getadc0")}
+def getadc1(){sendtodevice("getadc1")}
+def getadc4(){sendtodevice("getadc4")}
+def getadc5(){sendtodevice("getadc5")}
 
 def sendtodevice(String mystr){
     mystr=mystr.padRight(16,".") // mystr should be 16 bytes!  
